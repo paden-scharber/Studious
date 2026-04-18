eb8019bb9fd62891965612520ff2329403e09e61
bell-rachel <rachelbell703@gmail.com>
2026-04-17 21:24:51 -0500

    Updated card size, added delete entire set function to edit page, made go back button from edit page and study page back to all study sets

--- StudiousWeb/Models/StudyMaterial.cs
+++ StudiousWeb/Models/StudyMaterial.cs
@@ -1,10 +1,16 @@
 ﻿namespace StudiousWeb.Models
 {
-    public class StudyMaterial
+    public class StudyMaterialToEdit
     {
-        public string USERNAME { get; set; }
-        public string STUDYSET_NAME { get; set; }
-        public string TERM { get; set; }
-        public string DEFINITION { get; set; }
+        public int ID { get; set; }
+        public string USERNAME { get; set; } = "";
+        public string STUDYSET_NAME { get; set; } = "";
+        public string TERM { get; set; } = "";
+        public string DEFINITION { get; set; } = "";
+        public bool EDIT_MODE { get; set; } = false;
+        public bool IS_NEW { get; set; } = false;
+        public bool IS_DIRTY { get; set; } = false;
+        public string ORIGINAL_TERM { get; set; } = "";
+        public string ORIGINAL_DEFINITION { get; set; } = "";
     }
 }
